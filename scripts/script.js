c3f6a1e3ed7dad295d83ab85b52f36e8d23a14de
Cristina Alzuri <cristinaalzuri@gmail.com>
2023-05-04 16:03:46 -0400

    added current day function to js file

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,15 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+// This line indicates that the code is waiting for the document to be fully loaded before it runs.
+$(document).ready(function () {
+  // This line logs the current date and time in the console in the format of "Day of the week, Month name Day number
+  console.log(dayjs().format("dddd, MMMM D"))
+  // This line sets the text of the element with the ID "currentDay" to the same current date and time format as the console log.
+  $("#currentDay").text(dayjs().format("dddd, MMMM D"))
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
