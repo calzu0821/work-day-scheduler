1060749aef0b55330580b09cb0c003b4d6e18575
Cristina Alzuri <cristinaalzuri@gmail.com>
2023-05-04 17:37:13 -0400

    added code to get any user input that was saved in localStorage

--- scripts/script.js
+++ scripts/script.js
@@ -4,43 +4,42 @@
 
 // TODO: Add code to display the current date in the header of the page.
 // This line indicates that the code is waiting for the document to be fully loaded before it runs.
-  $(document).ready(function () {
-    var currentDate = dayjs();
-    var date = currentDate.date();
-    var suffix = getDayOfMonthSuffix(date);
+$(document).ready(function () {
+  var currentDate = dayjs();
+  var date = currentDate.date();
+  var suffix = getDayOfMonthSuffix(date);
 
-    // This line logs the current date and time in the console in the format of "Day of the week, Month name Day number
-    console.log(currentDate.format("dddd, MMMM D") + suffix);
-    // This line sets the text of the element with the ID "currentDay" to the same current date and time format as the console log.
-    $("#currentDay").text(currentDate.format("dddd, MMMM D") + suffix);
-  });
+  // This line logs the current date and time in the console in the format of "Day of the week, Month name Day number
+  console.log(currentDate.format("dddd, MMMM D") + suffix);
+  // This line sets the text of the element with the ID "currentDay" to the same current date and time format as the console log.
+  $("#currentDay").text(currentDate.format("dddd, MMMM D") + suffix);
+});
   
-  // This function adds a suffix to the current date.
-  function getDayOfMonthSuffix(day) {
-    if (day >= 11 && day <= 13) {
-      return 'th';
-    }
-    switch (day % 10) {
-      case 1:
-        return 'st';
-      case 2:
-        return 'nd';
-      case 3:
-        return 'rd';
-      default:
-        return 'th';
-    }
+// This function adds a suffix to the current date.
+function getDayOfMonthSuffix(day) {
+  if (day >= 11 && day <= 13) {
+    return 'th';
   }
+  switch (day % 10) {
+    case 1:
+      return 'st';
+    case 2:
+      return 'nd';
+    case 3:
+      return 'rd';
+    default:
+      return 'th';
+  }
+}
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+// TODO: Add a listener for click events on the save button. This code should
+// use the id in the containing time-block as a key to save the user input in
+// local storage. HINT: What does `this` reference in the click listener
+// function? How can DOM traversal be used to get the "hour-x" id of the
+// time-block containing the button that was clicked? How might the id be
+// useful when saving the description in local storage?
 
   $(".saveBtn").on("click", function() {
-    
     // Gets user input entered.
     var userTask = $(this).siblings(".description").val();
     var timeBlock = $(this).parent().attr("id");
@@ -53,11 +52,11 @@
   });
   
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+// TODO: Add code to apply the past, present, or future class to each time
+// block by comparing the id to the current hour. HINTS: How can the id
+// attribute of each time-block be used to conditionally add or remove the
+// past, present, and future classes? How can Day.js be used to get the
+// current hour in 24-hour time?
 
   // 1 - Get the current hour.
   var currentHour = dayjs().hour();
@@ -80,7 +79,17 @@ $(".time-block").each(function () {
   }
 });
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+// TODO: Add code to get any user input that was saved in localStorage and set
+// the values of the corresponding textarea elements. HINT: How can the id
+// attribute of each time-block be used to do this?
 
+// Access the users input saved in localStorage
+$('#hour-9 .description').val(JSON.parse(localStorage.getItem("hour-9")));
+$('#hour-10 .description').val(JSON.parse(localStorage.getItem("hour-10")));
+$('#hour-11 .description').val(JSON.parse(localStorage.getItem("hour-11")));
+$('#hour-12 .description').val(JSON.parse(localStorage.getItem("hour-12")));
+$('#hour-13 .description').val(JSON.parse(localStorage.getItem("hour-13")));
+$('#hour-14 .description').val(JSON.parse(localStorage.getItem("hour-14")));
+$('#hour-15 .description').val(JSON.parse(localStorage.getItem("hour-15")));
+$('#hour-16 .description').val(JSON.parse(localStorage.getItem("hour-16")));
+$('#hour-17 .description').val(JSON.parse(localStorage.getItem("hour-17")));
